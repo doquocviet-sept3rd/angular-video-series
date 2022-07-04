b01f501aab572261e69c2c7f6f99c82941fd4cbd
doquocviet-sept3rd <doquocviet.sept3rd@gmail.com>
2022-07-04 17:35:30 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+
+    <p [attr.attribute-you-are-tageting]='1 + 1 + "expression"'></p>
+    <tr>
+      <td [attr.colspan]='1 + 1'>attribute</td>
+    </tr>
+
+    <div [class.sale]='onSale'></div>
+    <div [class]='"classOne classTwo classThree"'></div>
+    <div [class]='["classOne", "classTwo", "classThree"]'></div>
+    <div [class]='{classOne: true, classTwo: true}'></div>
+
+    <div [style]='"width: 300px; height: 300px; background-color: red"'></div>
+    <div [style.width]='width'></div>
+    <div [style.background-color]='red'></div>
+    <div [style]='{width: "50px", height: "50px", backgroundColor: "red"}'></div>
+
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-video-series';
+  onSale = true;
+  width = 1;
+  red = 'red';
 }
