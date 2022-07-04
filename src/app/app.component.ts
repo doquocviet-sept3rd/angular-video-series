2c8b07e27a39e4ab75dc00121ff557c63b06d27e
doquocviet-sept3rd <doquocviet.sept3rd@gmail.com>
2022-07-04 15:30:24 +0700

    hello-world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-video-series';
+export class AppComponent implements OnInit, OnDestroy {
+
+  intervalSub: any;
+
+  ngOnInit() {
+    this.intervalSub = setInterval(() => {
+      console.log('Hello from ngOnInit');
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if (this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
 }
