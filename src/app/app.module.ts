c18c23689afcdb86a78a7f8cdd0c63bdd86e4fbb
doquocviet-sept3rd <doquocviet.sept3rd@gmail.com>
2022-07-04 17:08:21 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GreetingsPipePipe } from './greetings-pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreetingsPipePipe
   ],
   imports: [
     BrowserModule
