1b803c9145f2cd452d185da3bbda76a7f24674b4
doquocviet-sept3rd <doquocviet.sept3rd@gmail.com>
2022-07-04 16:59:23 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-video-series';
+
+  showText = false;
+
+  toggleText(event: any): void {
+    this.showText = !this.showText;
+    console.log(event);
+  }
 }
