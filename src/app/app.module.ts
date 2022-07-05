5a67fec3850b4ae25f712f1629c4a6227929c8c9
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2022-07-05 09:11:32 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppSizerComponent } from './app-sizer/app-sizer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppSizerComponent
   ],
   imports: [
     BrowserModule
