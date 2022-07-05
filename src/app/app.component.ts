b59e7b99aecbc12906b2ee42d30f238203881fe9
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2022-07-05 10:16:01 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { LogService } from './log.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-video-series';
+
+  constructor(
+    private logService: LogService,
+    private renderer: Renderer2,
+    private host: ElementRef
+  ) {}
+
+  ngOnInit() {
+    this.logService.logMessage('Hello');
+    this.renderer.setStyle(this.host.nativeElement, 'color', 'red');
+  }
 }
