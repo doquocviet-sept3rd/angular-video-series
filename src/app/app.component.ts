b90aed8c87fcdb7db2a3a12d0ba93b351564d234
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2022-07-05 09:26:03 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <input #phone placeholder='phone' type='text' />
+    <button (click)='callPhone(phone.value)'>Call</button>
+
+    <input #ref1 type='text' [(ngModel)]='inputValue' />
+    <span *ngIf='true'>{{ref1.value}}</span>
+
+    <input #ref1 type='text' [(ngModel)]='inputValue' />
+    <ng-template [ngIf]='true'>
+      <span>{{ref1.value}}</span>
+    </ng-template>
+
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-video-series';
+  inputValue: string = '';
+
+  callPhone(phone: string) {
+    window.alert(phone);
+  }
 }
