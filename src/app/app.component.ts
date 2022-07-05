19f73ef83e97ad3d1d1df83df39993bd3b92d5a0
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2022-07-05 10:10:10 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div *ngIf='isActive'>Active</div>
+
+    <div *ngFor='let item of items; let i = index'>
+      <div>{{i}} - {{item.name}}</div>
+    </div>
+
+    <div [ngSwitch]='parent'>
+      <div *ngSwitchCase='"bob"'>Hi Bob</div>
+      <div *ngSwitchCase='"bar"'>Hi Bar</div>
+      <div *ngSwitchDefault>Hi ho</div>
+    </div>
+
+  `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-video-series';
+  isActive: boolean = true;
+
+  items: { name: string }[] = [
+    { name: 'bob' },
+    { name: 'foo' },
+    { name: 'bar' }
+  ];
+
+  parent: string = '1';
+
+  ngOnInit() {
+    setInterval(() => {
+      this.isActive = Date.now().valueOf() % 2 === 0;
+    }, 1000);
+  }
 }
