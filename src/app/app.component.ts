b284a1aa24e3faef632a9bc3d50587718b108423
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2022-07-05 09:43:53 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+
+    <div [ngClass]='isSpecial ? "special" : ""'>Example1</div>
+    <div [ngClass]='currenClasses'>Example2</div>
+    <div [ngStyle]='currentStyles'>Example3</div>
+
+    <label for='example'>{{name}}</label>
+    <input [(ngModel)]='name' id='example' />
+
+  `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  name: string = 'label';
+
+  ngOnInit(): void {
+    this.setCurrentClass();
+    this.setCurrentStyles();
+  }
+
   title = 'angular-video-series';
+  isSpecial: boolean = true;
+  currenClasses: Object = {};
+  currentStyles: Object = {};
+
+  setCurrentClass() {
+    this.currenClasses = {
+      savable: true,
+      modified: false,
+      special: true
+    };
+  }
+
+  setCurrentStyles() {
+    this.currentStyles = {
+      'font-style': 'italic',
+      'font-weight': '700'
+    };
+  }
+
 }
