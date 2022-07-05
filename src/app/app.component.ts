e127981f63d97ed90c19fa5dacb22ec55bca0db7
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2022-07-05 12:20:25 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-video-series';
+  name = new FormControl('');
+
+  updateName() {
+    this.name.setValue('Movie');
+  }
+
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl('')
+  });
+
+  onSubmit() {
+    console.warn(this.profileForm.value);
+  }
+
 }
